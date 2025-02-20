5ddbe1c1358bbd3853920651628a32a66e4bccc4
Hadil Hammami <hadilhammami670@gmail.com>
2025-02-20 23:25:18 +0100

    pushing project

---
+++ src/test/java/esprit/project/aiagent/AiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package esprit.project.aiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
