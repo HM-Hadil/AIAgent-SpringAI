7f5033c35d37caffa68ac42b8c4bb1fd2bb18151
Hadil Hammami <hadilhammami670@gmail.com>
2025-03-12 22:44:19 +0100

    adding jira client rest api

--- src/main/java/springai/project/aiagent/controller/JiraController.java
+++ src/main/java/springai/project/aiagent/controller/JiraController.java
@@ -1,27 +1,64 @@
 package springai.project.aiagent.controller;
 
+import com.atlassian.jira.rest.client.api.domain.Comment;
+import com.atlassian.jira.rest.client.api.domain.Issue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import springai.project.aiagent.service.JiraClientService;
+import springai.project.aiagent.service.MyJiraClient;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/jira")
 public class JiraController {
-    private final JiraClientService jiraClientService;
 
-    public JiraController(JiraClientService jiraClientService) {
-        this.jiraClientService = jiraClientService;
+    private final MyJiraClient myJiraClient;
+
+    @Autowired
+    public JiraController(MyJiraClient myJiraClient){
+        this.myJiraClient = myJiraClient;
     }
 
-    @GetMapping("/projects")
-    public String getProjects() {
-        return jiraClientService.getAllProjects();
+    @PostMapping("/createIssue")
+    public ResponseEntity<String> createIssue(@RequestParam String projectKey,
+                                              @RequestParam Long issueType,
+                                              @RequestParam String issueSummary) {
+        String issueKey = myJiraClient.createIssue(projectKey, issueType, issueSummary);
+        return ResponseEntity.ok(issueKey);
     }
 
-    @PostMapping("/create-issue")
-    public String createIssue(@RequestParam String projectKey,
-                              @RequestParam String summary,
-                              @RequestParam String description,
-                              @RequestParam String issueType) {
-        return jiraClientService.createIssue(projectKey, summary, description, issueType);
+    @PutMapping("/updateIssueDescription")
+    public ResponseEntity<Void> updateIssueDescription(@RequestParam String issueKey,
+                                                       @RequestParam String newDescription) {
+        myJiraClient.updateIssueDescription(issueKey, newDescription);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/issue/{issueKey}")
+    public ResponseEntity<Issue> getIssue(@PathVariable String issueKey) {
+        Issue issue = myJiraClient.getIssue(issueKey);
+        return ResponseEntity.ok(issue);
+    }
+
+    @DeleteMapping("/issue/{issueKey}")
+    public ResponseEntity<Void> deleteIssue(@PathVariable String issueKey,
+                                            @RequestParam(defaultValue = "false") boolean deleteSubtasks) {
+        myJiraClient.deleteIssue(issueKey, deleteSubtasks);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/addComment")
+    public ResponseEntity<Void> addComment(@RequestParam String issueKey,
+                                           @RequestParam String commentBody) {
+        Issue issue = myJiraClient.getIssue(issueKey);
+        myJiraClient.addComment(issue, commentBody);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/comments/{issueKey}")
+    public ResponseEntity<List<Comment>> getAllComments(@PathVariable String issueKey) {
+        List<Comment> comments = myJiraClient.getAllComments(issueKey);
+        return ResponseEntity.ok(comments);
     }
 }
